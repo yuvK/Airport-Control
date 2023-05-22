d9ebe9eb132d8f3a64b05c8ba0845c58b59ef6a9
yuvK <83789663+yuvK@users.noreply.github.com>
2023-05-22 12:32:30 +0300

    Add files via upload

--- Airport.UI/src/App.tsx
+++ Airport.UI/src/App.tsx
@@ -4,16 +4,16 @@ import { useSignalR } from './Hooks/useSignalR'
 import { AirportService } from './Service/AirportService'
 import { IStationDTO } from './Interfaces/IStationDTO'
 import { IAirplaneDTO } from './Interfaces/IAirplaneDTO'
-import {Map} from './components/Map/Map'
+import { Map } from './components/Map/Map'
 import { FlightsBoard } from './components/FlightsBoard/FlightsBoard'
 
 export const airportContext = createContext<{
   stationDataQueue: IStationDTO[];
-  flightsList:IAirplaneDTO[];
-  
+  flightsList: IAirplaneDTO[];
+
   getStatus: () => Promise<any>;
 
-  
+
 }>({} as any);
 function App() {
   //const {startTimer, count} = useSignalR();
@@ -26,7 +26,10 @@ function App() {
 
   return (<>
     <main>
-      <h1>Airport Simulator</h1>
+      <div className='title-container'>
+        <img className='logo' src="public/ct.png"></img>
+        <h1 className='title'>Airport Simulator</h1>
+      </div>
       <airportContext.Provider value={{ ...signal, ...airportService }}>
         <Map />
         <FlightsBoard />
